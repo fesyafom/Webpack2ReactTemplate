764e4709b16fa6e59a78f3a48489950e8ac6f5da
fesyafom <a.feschenko@protonmail.com>
2017-06-04 19:16:03 +0300

    +eslint

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,12 @@ const config = {
     module: {
       rules: [
           {
+              enforce: "pre",
+              test: /\.(js|jsx)/,
+              exclude: /node_modules/,
+              loader: "eslint-loader"
+          },
+          {
               test: /\.(js|jsx)/,
               exclude: /node_modules/,
               loader: "babel-loader"
